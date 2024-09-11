b6a663c5cf5d74fdf301d18bc5696733e2313168
ladisamuel <ladisamuel00@gmail.com>
2024-09-10 17:00:36 -0700

    update

--- src/pages/staff/DashboardHome.jsx
+++ src/pages/staff/DashboardHome.jsx
@@ -16,24 +16,26 @@ export default function DashboardHome() {
                         <p className="text-sm my-4 ">Emmanuel</p>
                         <p className='text-xs text-gray-400 '>The Embassy of the Kingdom of Heaven (EKOH)</p>
                     </div>
-                    <div className="">
-                        <div>
-                            <div>
+                    <div className=" w-[65%] m-auto mt-4">
+                        <div className="flex justify-between">
+                            <div className="border p-5 rounded-2xl bg-[#E3F5FF]">
                                 <p>Total Data Entry</p>
                                 <p>721K</p>
                             </div>
-                            <div>
+                            <div className="border p-5 rounded-2xl bg-[#E5ECF6]">
                                 <p>Total Approved</p>
                                 <p>367K</p>
                             </div>
-                            <div>
+                            <div className="border p-5 rounded-2xl bg-[#E3F5FF]">
                                 <p>Total Pending</p>
                                 <p>354k</p>
                             </div>
                         </div>
 
-                        <div onClick={() => navigate('/AddNewOne')} className='flex items-center gap-3 text-xs ml-5 px-2 py-2 bg-[#4A3AFF] rounded-lg text-white cursor-pointer'>
-                            <p className=''>Add New Entry</p>
+                        <div className="w-fit m-auto">
+                            <button onClick={() => navigate('/AddNewOne')} className='text-center mt-4 border text-xs px-5 py-2 bg-[#4A3AFF] ml-4 rounded-lg text-white cursor-pointer w-fit'>
+                                Create a new entry
+                            </button>
                         </div>
                     </div>
                 </div>
